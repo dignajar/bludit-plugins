976798a3e6f9115a23c325b6b320ee17ac565317
Frédéric K <stradfred@gmail.com>
2015-12-07 11:27:45 +0100

    Update Contact 0.5
    More compatibility with Bludit 0.7

--- contact/layout/contact.php
+++ contact/layout/contact.php
@@ -12,11 +12,12 @@
 ?>
 <form method="post" action="<?php echo $Site->url(). $Page->slug(); ?>" class="pure-form">
 		<fieldset class="pure-group">
-			<input type="hidden" name="token" value="<?php echo pluginContact::generateToken(true); ?>">
-			<input id="name" type="text" name="name" value="<?php echo pluginContact::cleanString($name); ?>" placeholder="<?php echo $Language->get('Name'); ?>" class="pure-input-1-2">
-			<input id="email" type="email" name="email" value="<?php echo pluginContact::cleanString($email); ?>" placeholder="<?php echo $Language->get('Email'); ?>" class="pure-input-1-2">
-			<textarea id="message" rows="6" name="message" placeholder="<?php echo $Language->get('Message'); ?>" class="pure-input-1-2"><?php echo pluginContact::cleanString($message); ?></textarea>
+			<input type="hidden" name="token" value="<?php echo $Security->printTokenCSRF(); ?>">
+			<input id="name" type="text" name="name" value="<?php echo sanitize::html($name); ?>" placeholder="<?php echo $Language->get('Name'); ?>" class="pure-input-1-2">
+			<input id="email" type="email" name="email" value="<?php echo sanitize::email($email); ?>" placeholder="<?php echo $Language->get('Email'); ?>" class="pure-input-1-2">
+			<textarea id="message" rows="6" name="message" placeholder="<?php echo $Language->get('Message'); ?>" class="pure-input-1-2"><?php echo sanitize::html($message); ?></textarea>
 		</fieldset>
 		<input type="checkbox" name="interested" style="display: none;">
 		<button id="submit" name="submit" type="submit" class="pure-button pure-button-primary"><?php echo $Language->get('Send'); ?></button>
+		<button type="reset" class="pure-button"><?php echo $Language->get('Reset'); ?></button>
 </form>
