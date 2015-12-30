81aa7c9e179f9360a731b7db7803a62b3856cfd3
Edi <egoetschel@clickwork.ch>
2015-12-30 02:26:27 +0100

    Update pagination.php
    Fix for installations in a subdirectory.

--- pagewithposts/includes/pagination.php
+++ pagewithposts/includes/pagination.php
@@ -1,26 +1,27 @@
 <script type="text/javascript">
 
 <?php
-  $key = $Page->key();
-  echo "var key = '{$key}';";
+$key = $Page->key();
+echo "var key = '{$key}';";
 ?>
 
 if ( typeof $(".right").html() != "undefined" ) {
 
-	var next = $(".right").html();
-	var nextnew = '<li class="right">' + next.slice(0, 10) + key + next.slice(10) + '</li>';
+var next = $(".right").html();
+var n = next.indexOf("?page");
+var nextnew = '<li class="right">' + next.slice(0, n) + key + next.slice(n) + '</li>';
 
-	$( ".right" ).replaceWith( nextnew );
+$( ".right" ).replaceWith( nextnew );
 
 }
 
 if ( typeof $(".left").html() != "undefined" ) {
 
-	var prev = $(".left").html();
-	var prevnew = prev.slice(0, 10) + key + prev.slice(10);
+var prev = $(".left").html();
+var m = prev.indexOf("?page");
+var prevnew = prev.slice(0, m) + key + prev.slice(m);
 
-
-	$( ".left" ).replaceWith( "<li class=\"left\">" + prevnew + "</li>" );
+$( ".left" ).replaceWith( "<li class=\"left\">" + prevnew + "</li>" );
 
 }
 
