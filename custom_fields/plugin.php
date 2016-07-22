f4987906a8ea147e2a8626cde5b2b5719ac94517
janxb <github@janbrodda.de>
2016-07-22 16:59:29 +0200

    Added first Version of custom_fields plugin.

---
+++ custom_fields/plugin.php
@@ -0,0 +1,106 @@
+<?php
+
+class pluginCustomFields extends Plugin
+{
+	private $fields;
+	const FIELD_KEY = 'customFields';
+
+	private $addedFields = array();
+
+	public function init()
+	{
+		$this->dbFields = array(
+			$this::FIELD_KEY => '{}'
+		);
+
+		$GLOBALS[$this::FIELD_KEY] = $this;
+	}
+
+	public function load()
+	{
+		$fieldsJson = $this->getDbField($this::FIELD_KEY, false);
+		$this->fields = json_decode($fieldsJson);
+	}
+
+	public function save()
+	{
+		if (!$this->installed())
+			return;
+
+		// Remove empty fields, which are not in use by templates
+		foreach ($this->fields as $key => $value) {
+			if (!in_array($key, $this->addedFields))
+				unset($this->fields->$key);
+		}
+
+		$fieldsJson = json_encode($this->fields);
+		$this->dbFields[$this::FIELD_KEY] = $fieldsJson;
+		$this->setDb($this->dbFields);
+	}
+
+	public function form()
+	{
+		$html = '';
+
+		$html .= '<div>';
+		$html .= '<input type="hidden" id="fieldJson" name="' . $this::FIELD_KEY . '" value="' . $this->getDbField($this::FIELD_KEY) . '" />';
+		$html .= '</div>';
+		$html .= '<div id="fieldInputs"></div>';
+
+		$html .= '
+		<script>
+			var $jsonInput = $("#fieldJson");
+			var $inputContainer = $("#fieldInputs");
+			var $form = $jsonInput.closest("form");
+			var $data = JSON.parse($jsonInput.val());
+			$.each($data, function( index, value ) {
+  				$inputContainer.append("<label><b>"+index+"</b></label>"); 
+  				$inputContainer.append("<input type=\'text\' name=\'"+index+"\' value=\'"+value+"\'>"); 
+  				$inputContainer.append("<br><br>"); 
+			});
+			
+			$form.submit(function(){
+				var newData = {};
+
+				$.each($inputContainer.children("input"), function( index, input ) {
+					$input = $(input);
+					newData[$input.attr("name")] = $input.val();
+				});
+				
+				$jsonInput.val(JSON.stringify(newData));
+			})
+		</script>';
+
+		return $html;
+	}
+
+	public function get($key)
+	{
+		if (!$this->installed())
+			return false;
+
+		if (!isset($this->fields))
+			$this->load();
+
+		if (!isset($this->fields->$key))
+			$this->add($key);
+
+		return $this->fields->$key;
+	}
+
+	public function add($key, $defaultValue = '')
+	{
+		if (!$this->installed())
+			return false;
+
+		array_push($this->addedFields, $key);
+
+		if (!isset($this->fields))
+			$this->load();
+
+		if (!isset($this->fields->$key)) {
+			$this->fields->$key = $defaultValue;
+			$this->save();
+		}
+	}
+}
