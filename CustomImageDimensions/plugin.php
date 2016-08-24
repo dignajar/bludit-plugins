89eb340916450ce55984b3ae81691b5dbd0fdc31
Alexandre Teles <ateles.1@gmail.com>
2016-08-24 18:26:39 -0300

    Update formating

--- CustomImageDimensions/plugin.php
+++ CustomImageDimensions/plugin.php
@@ -7,31 +7,31 @@ class pluginCustomImageDimensions extends Plugin {
 		$pluginPath = $this->htmlPath();
 		$html  = '<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.slim.min.js"></script>';
 		$html .= '
-        <script>
-            $("img").each(function(){  
-                imgAlt = this.alt;
-                if(/height=/i.test(imgAlt)){
-                    heightPosition = imgAlt.lastIndexOf("height=") + 7;
-                    height = heightPosition;
-                    for(i = heightPosition; i < imgAlt.length; i++){
-                        if(!isNaN(parseInt(imgAlt.substring(i)))){
-                            height += 1;
-                        } else break;
-                    }
-                    this.style.setProperty("height", imgAlt.substring(heightPosition, height));
-                }
-                if(/width=/i.test(imgAlt)){
-                    widthPosition = imgAlt.lastIndexOf("width=") + 6;
-                    width = widthPosition;
-                    for(i = widthPosition; i < imgAlt.length; i++){
-                        if(!isNaN(parseInt(imgAlt.substring(i)))){
-                            width += 1;
-                        } else break;
-                    }
-                    this.style.setProperty("width", imgAlt.substring(widthPosition, width));
-                }
-			});
-        </script>
+		        <script>
+		            $("img").each(function(){  
+		                imgAlt = this.alt;
+		                if(/height=/i.test(imgAlt)){
+		                    heightPosition = imgAlt.lastIndexOf("height=") + 7;
+		                    height = heightPosition;
+		                    for(i = heightPosition; i < imgAlt.length; i++){
+		                        if(!isNaN(parseInt(imgAlt.substring(i)))){
+		                            height += 1;
+		                        } else break;
+		                    }
+		                    this.style.setProperty("height", imgAlt.substring(heightPosition, height));
+		                }
+		                if(/width=/i.test(imgAlt)){
+		                    widthPosition = imgAlt.lastIndexOf("width=") + 6;
+		                    width = widthPosition;
+		                    for(i = widthPosition; i < imgAlt.length; i++){
+		                        if(!isNaN(parseInt(imgAlt.substring(i)))){
+		                            width += 1;
+		                        } else break;
+		                    }
+		                    this.style.setProperty("width", imgAlt.substring(widthPosition, width));
+		                }
+					});
+		        </script>
 		';
 		
 		return $html;	
