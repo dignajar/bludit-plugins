d00ec7486df04b07d412ed6a1cf26bedec865345
lhenriksson <contact@lhenriksson.se>
2017-08-07 17:11:24 +0200

    Added lastfm plugin

---
+++ lastfm/update.php
@@ -0,0 +1,45 @@
+<?php
+
+	$user = $_POST['u'];
+	$key = $_POST['k'];
+	$text = $_POST['t'];
+
+        if(empty($user) | empty($key)) return false;
+    		$track = getTrackInfo($user, $key);
+
+			if(!$track) return false;
+
+			$pluginText = preg_replace_callback("/{(\w+)}/", function($m) use($track){
+				return $track[$m[1]];
+			}, $text);
+
+			echo $pluginText;
+			
+		function getTrackInfo($user, $key) {
+        $lastFmResponse = makeRequest($user, $key);
+        if (!count($lastFmResponse['recenttracks'])) {
+            return false;
+        }
+			
+        $lastTrack = $lastFmResponse['recenttracks']['track'][0];
+
+        return [
+            'artist' => $lastTrack['artist']['#text'],
+            'track' => $lastTrack['name'],
+            'album' => $lastTrack['album']['#text'],
+            'tracklink' => '<a href="'.$lastTrack['url'].'">'.$lastTrack['name'].'</a>'
+        ];
+    }
+
+		function makeRequest($user, $key) {
+        $url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1&user='.$user.'&api_key='.$key.'&format=json&limit=1';
+        $response = file_get_contents($url);
+        return json_decode($response, true);
+}
+
+
+
+
+
+
+
