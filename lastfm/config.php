f7c55bf1034792bd067f83dde8ada7f384c2170e
lhenriksson <contact@lhenriksson.se>
2017-08-08 08:09:19 +0200

    Smallfix

--- lastfm/config.php
+++ lastfm/config.php
@@ -1,9 +1,1 @@
 <?php
-$config = array (
-  'apikey' => 'e835ada62694b1add631ea11d9d54331',
-  'username' => 'lhenriksson',
-  'plugintext' => '♪ Now playing {tracklink} by {artist} from {album}',
-  'showLatest' => '0',
-  'position' => 'bodyBegin',
-  'autoupdate' => '0',
-);
